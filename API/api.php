daab2debf56db5b23ea9f782913d9a6398c6f902
Sladeck <gm.moulin@hotmail.fr>
2017-06-23 11:39:45 +0200

    [ADD] API

---
+++ API/api.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once 'config.php';
+
+
+
+function getUserById($pdo, $id) {
+  $sql = "SELECT * FROM `users` WHERE `id` = ".$id;
+  $exe = $pdo->query($sql);
+
+  while($result = $exe->fetch(PDO::FETCH_OBJ)) {
+      $user = array("pseudo"    => $result->pseudo,
+                    "firstName" => $result->firstname,
+                    "id"        => $result->id,
+                    "password"  => $result->password);
+  }
+      return $user;
+}
+
+function getUsers($pdo){
+  $sql = "SELECT * FROM `users`";
+  $exe = $pdo->query($sql);
+  $listUsers = array();
+
+  while($result = $exe->fetch(PDO::FETCH_OBJ)) {
+    array_push($listUsers, array("pseudo"    => $result->pseudo,
+                                 "firstName" => $result->firstname,
+                                 "id"        => $result->id,
+                                 "password"  => $result->password));
+  }
+
+  return $listUsers;
+
+}
+
+
+$possible_url = array("get_user","get_users");
+$value ="Une erreur est survenue !";
+
+if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url)) { //si l'URL est OK
+  switch ($_GET["action"]) {
+    case "get_user":
+      if (isset($_GET["id"])) {
+        $value = getUserById($_GET["id"], $pdo); break;
+      } else {
+        $value = "Argument manquant"; break;
+      }
+    case "get_users":
+      $value = getUsers($pdo); break;
+  }
+  exit(json_encode($value));
+}
