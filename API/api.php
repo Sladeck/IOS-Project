a67241a50ee0bafc369d55b672534e90ee3ad828
Laurie <lauries@outlook.fr>
2017-07-10 14:32:45 +0200

    [UPDATE] API CRUD for todo

--- API/api.php
+++ API/api.php
@@ -30,7 +30,87 @@ function getUsers($pdo){
 }
 
 
-$possible_url = array("get_user","get_users");
+
+function create($pdo, $pseudo, $content, $title){
+    $contenu = $content
+    if($pseudo == "" || $contenu == "" || $title == ""){
+      $content = json_encode(["result" => []]);
+    }else {
+      $tabResult = [];
+      $contenu = str_replace("'", "''", $contenu);
+      $query = "INSERT INTO `todo`(`pseudo`, `content`, `title`) VALUES ('$pseudo','$contenu','$title')";
+      $queryExect = $pdo->prepare($query);
+      $queryExect->execute();
+      $result = $queryExect->fetch();
+      $tabResult = [
+          0 => [
+              "id" => $pdo->lastInsertId(),
+              "pseudo" => $pseudo,
+              "content" => $contenu,
+              "title" => $title
+          ]
+      ];
+      $content = json_encode(["result" => $tabResult]);
+    }
+}
+
+
+function delete($pdo, $id){
+    if($id == ""){
+      $content = json_encode(["result" => []]);
+    }else {
+      $tabResult = [];
+      $query = "DELETE FROM todo WHERE `id` = $id";
+      $queryExect = $pdo->prepare($query);
+      $queryExect->execute();
+      $content = json_encode(["result" => ["id" => $id]]);
+    }
+}
+
+function update($pdo, $pseudo, $content, $title, $id){
+    $contenu = $content
+    if($pseudo == "" || $contenu == "" || $title == "" || $id == ""){
+        $content = json_encode(["result" => []]);
+    }else {
+        $tabResult = [];
+        $contenu = str_replace("'", "''", $contenu);
+        $query = "UPDATE `todo` SET `id`='$id',`pseudo`='$pseudo',`content`='$contenu', `title`='$title' WHERE `id` = $id";
+    $queryExect = $pdo->prepare($query);
+        $queryExect->execute();
+        $result = $queryExect->fetch();
+        $tabResult = [
+            0 => [
+                "id" => $id,
+                "pseudo" => $pseudo,
+                "content" => $contenu,
+                "title" => $title
+            ]
+        ];
+        $content = json_encode(["result" => $tabResult]);
+    }
+}
+
+function getAll($pdo, $pseudo){
+    if($pseudo == null){
+        $content = json_encode(["result" => []]);
+    }else {
+        $tabResult = [];
+        $query = "SELECT * FROM todo WHERE `pseudo` = '$pseudo'";
+        $responseQuery = $pdo->query($query)->fetchAll();
+        foreach ($responseQuery as $result) {
+            unset($result[0]);
+            unset($result[1]);
+            unset($result[2]);
+            unset($result[3]);
+            unset($result[4]);
+            $tabResult[] = $result;
+        }
+        $content = json_encode(["result" => $tabResult]);
+    }
+}
+
+
+$possible_url = array("get_user","get_users","create","delete","update","getAll");
 $value ="Une erreur est survenue !";
 
 if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url)) { //si l'URL est OK
@@ -43,6 +123,14 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url)) { //si l
       }
     case "get_users":
       $value = getUsers($pdo); break;
+    /* case "create":
+      $value = create($pdo, $_GET["pseudo"], $_GET["title"], $_GET["content"]); break;
+    case "delete":
+      $value = delete($pdo, $_GET["id"]); break;
+    case "update"
+      $value = update($pdo, $_GET["pseudo"], $_GET["content"], $_GET["title"], $_GET["id"]); break;
+    case "getAll"
+      $value= getAll($pdo, $_GET["pseudo"]); break; */
   }
   exit(json_encode($value));
 }
--- API/users.sql
+++ API/users.sql
@@ -1,24 +1,31 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.4.10
+-- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Ven 23 Juin 2017 à 08:32
--- Version du serveur :  5.7.14
--- Version de PHP :  7.0.10
+-- Client :  localhost
+-- Généré le :  Lun 10 Juillet 2017 à 14:24
+-- Version du serveur :  5.5.42
+-- Version de PHP :  7.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+--
+-- Base de données :  `bdd`
+--
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- --------------------------------------------------------
 
 --
--- Base de données :  `ios`
+-- Structure de la table `todo`
 --
+
+CREATE TABLE `todo` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -27,14 +34,48 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(11) NOT NULL,
   `pseudo` varchar(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `pseudo`, `firstname`, `lastname`, `email`, `password`) VALUES
+(1, 'LattyS', 'Laurie', 'Seiller', 'lattys@gmail.com', 'azerty');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `todo`
+--
+ALTER TABLE `todo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `todo`
+--
+ALTER TABLE `todo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
